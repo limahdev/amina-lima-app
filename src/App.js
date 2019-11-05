112638419f12b739ca7669812722d948f29f8bde
Lima Muhammad <limah.dev@gmail.com>
2019-11-05 13:45:57 -0800

    final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Amima';
+
 function App() {
+  const [imageUrl, setImageUrl] = useState(
+    "https://media0.giphy.com/media/l3q2P2l54336ew5iM/giphy.webp"
+  );
+  
+  function handleClick() {
+    window.alert("hello Amima!");
+  }
+
+  function fetchImage() {
+    console.log("hello again!");
+    fetch(
+      "http://api.giphy.com/v1/gifs/random?api_key=aHi5kZ49h4esZTalKqyvkBO2llvuHeJr&tag=funny"
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        const newImageUrl = myJson.data.images.original.url;
+        setImageUrl(newImageUrl);
+      });
+  }
+
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{name} Giphy Shuffle App!</h1>
+        <img src={imageUrl} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This App was created by {name}!!! 
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +47,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={fetchImage}>Shuffle!</button>
       </header>
     </div>
   );
